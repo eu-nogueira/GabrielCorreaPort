ee09b0d30526a1c342b10885198a0a32cb663364
eu-nogueira <marcosnogueirapro@gmail.com>
2025-04-10 01:45:29 -0300

    Ajuste img 5.0

--- PortfolioGB/src/components/PicturesLifesty.jsx
+++ PortfolioGB/src/components/PicturesLifesty.jsx
@@ -6,27 +6,27 @@ function PicturesLifesty() {
     <div style={{backgroundColor: 'rgba(0, 0, 0, 0.8)'}}>
     <div className="picturesSty">
         <div className="picture">
-            <img src="src\assets\images\IMG_7278.jpg" alt="picture1" width={300}/>
+            <img src={"/static/images/IMG_7278.jpg"} alt="picture1" width={300}/>
         </div>
         <div className="picture">
-            <img src="src\assets\images\IMG_7275.jpg" alt="picture2" width={300}/>
+            <img src={"/static/images/IMG_7275.jpg"} alt="picture2" width={300}/>
         </div>
         <div className="picture">
-            <img src="src\assets\images\IMG_7303.jpg" alt="picture3" width={300}/>
+            <img src={"/static/images/IMG_7303.jpg"} alt="picture3" width={300}/>
         </div>
         <div className="picture">
-            <img src="src\assets\images\IMG_7267.jpg" alt="picture4" width={300}/>
+            <img src={"/static/images/IMG_7267.jpg"} alt="picture4" width={300}/>
         </div>
         <div className="picture">
-            <img src="src\assets\images\IMG_7289.jpg" alt="picture5" width={300}/>
+            <img src={"/static/images/IMG_7289.jpg"} alt="picture5" width={300}/>
         </div>
         <div className="picture">
-            <img src="src\assets\images\IMG_7292.jpg" alt="picture6" width={300}/>
+            <img src={"/static/images/IMG_7292.jpg"} alt="picture6" width={300}/>
         </div>
     </div>
     <div className="logoWhats">
         <a href="https://api.whatsapp.com/send?phone=5551997071097&text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20sobre%20seus%20pacotes%20de%20servi%C3%A7os.">
-            <img src="src\assets\images\10000485-removebg-preview.png" alt='Logo Whatsapp' width={50}/>
+            <img src={"/static/images/10000485-removebg-preview.png"} alt='Logo Whatsapp' width={50}/>
         </a>
     </div>
 </div>
--- PortfolioGB/src/components/PicturesRetrat.jsx
+++ PortfolioGB/src/components/PicturesRetrat.jsx
@@ -6,45 +6,45 @@ function PicturesRetrat() {
     <div style={{backgroundColor: 'rgba(0, 0, 0, 0.8)'}}>
         <div className="pictures">
             <div className="picture">
-                <img src="src\assets\images\IMG_7491.jpg" alt="picture1" width={300}/>
+                <img src={"/static/images/IMG_7491.jpg"} alt="picture1" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_8887.jpg" alt="picture2" width={300}/>
+                <img src={"/static/images/IMG_8887.jpg"} alt="picture2" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_8815.jpg" alt="picture3" width={300}/>
+                <img src={"/static/images/IMG_8815.jpg"} alt="picture3" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_9183.jpg" alt="picture4" width={300}/>
+                <img src={"/static/images/IMG_9183.jpg"} alt="picture4" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_2576.jpg" alt="picture5" width={300}/>
+                <img src={"/static/images/IMG_2576.jpg"} alt="picture5" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_7398.jpg" alt="picture6" width={300}/>
+                <img src={"/static/images/IMG_7398.jpg"} alt="picture6" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_7400.jpg" alt="picture7" width={300}/>
+                <img src={"/static/images/IMG_7400.jpg"} alt="picture7" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_8764.jpg" alt="picture8" width={300}/>
+                <img src={"/static/images/IMG_8764.jpg"} alt="picture8" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_9023.jpg" alt="picture9" width={300}/>
+                <img src={"/static/images/IMG_9023.jpg"} alt="picture9" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_9020.jpg" alt="picture10" width={300}/>
+                <img src={"/static/images/IMG_9020.jpg"} alt="picture10" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_7498.jpg" alt="picture11" width={300}/>
+                <img src={"/static/images/IMG_7498.jpg"} alt="picture11" width={300}/>
             </div>
             <div className="picture">
-                <img src="src\assets\images\IMG_9053.jpg" alt="picture12" width={300}/>
+                <img src={"/static/images/IMG_9053.jpg"} alt="picture12" width={300}/>
             </div>
         </div>
     <div className="logoWhats">
         <a href="https://api.whatsapp.com/send?phone=5551997071097&text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20sobre%20seus%20pacotes%20de%20servi%C3%A7os.">
-            <img src="src\assets\images\10000485-removebg-preview.png" alt='Logo Whatsapp' width={50}/>
+            <img src={"/static/images/10000485-removebg-preview.png"} alt='Logo Whatsapp' width={50}/>
         </a>
     </div>
     </div>
